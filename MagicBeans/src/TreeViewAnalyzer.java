427f3ffb5bee6aee5a4f513ad481291bb6be6bb0
AlexLaurent11 <LaurentAM03@uww.edu>
2015-12-05 11:52:03 -0600

    I don't remember doing this... Here we go

---
+++ MagicBeans/src/TreeViewAnalyzer.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Alex
+ */
+public class TreeViewAnalyzer {
+    public static void main(String args[]){
+        System.out.print("fuck off, give me an A");
+    }
+}
