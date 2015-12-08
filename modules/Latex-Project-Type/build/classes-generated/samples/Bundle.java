a18bf027129185252ca59b981a8a484af4149449
NickBolles <nickbolles@live.com>
2015-12-07 21:23:38 -0600

    Create the Latex Project type and supply a sample latex project structure

---
+++ modules/Latex-Project-Type/build/classes-generated/samples/Bundle.java
@@ -0,0 +1,13 @@
+package samples;
+/** Localizable strings for {@link samples}. */
+@javax.annotation.Generated(value="org.netbeans.modules.openide.util.NbBundleProcessor")
+class Bundle {
+    /**
+     * @return <i>Latex Starter</i>
+     * @see LatexStarter1WizardIterator
+     */
+    static String LatexStarter1_displayName() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "LatexStarter1_displayName");
+    }
+    private void Bundle() {}
+}
--- modules/Latex-Project-Type/src/org/magicbeans/project/LatexProjectFactory.java
+++ modules/Latex-Project-Type/src/org/magicbeans/project/LatexProjectFactory.java
@@ -3,35 +3,50 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package org.customer.project;
+package org.magicbeans.project;
 
 import java.io.IOException;
+import org.netbeans.api.annotations.common.StaticResource;
 import org.netbeans.api.project.Project;
 import org.netbeans.spi.project.ProjectFactory;
 import org.netbeans.spi.project.ProjectState;
+import org.netbeans.spi.project.ui.LogicalViewProvider;
 import org.openide.filesystems.FileObject;
+import org.openide.loaders.DataFolder;
+import org.openide.loaders.DataObjectNotFoundException;
+import org.openide.nodes.AbstractNode;
+import org.openide.nodes.Children;
+import org.openide.nodes.Node;
+import org.openide.util.Exceptions;
 import org.openide.util.lookup.ServiceProvider;
 
 @ServiceProvider(service=ProjectFactory.class)
-public class CustomerProjectFactory implements ProjectFactory {
+public class LatexProjectFactory implements ProjectFactory{
 
-    public static final String PROJECT_FILE = "customer.txt";
+    public static final String PROJECT_FILE = "*.tex";
 
     //Specifies when a project is a project, i.e.,
     //if "customer.txt" is present in a folder:
     @Override
     public boolean isProject(FileObject projectDirectory) {
-        return projectDirectory.getFileObject(PROJECT_FILE) != null;
+        FileObject children[] = projectDirectory.getChildren();
+        for (FileObject child:children){
+            if (child.getExt().equals("tex")){
+                return true;
+            }
+        }
+        return  false;
     }
 
     //Specifies when the project will be opened, i.e., if the project exists:
     @Override
     public Project loadProject(FileObject dir, ProjectState state) throws IOException {
-        return isProject(dir) ? new CustomerProject(dir, state) : null;
+        return isProject(dir) ? new LatexProject(dir, state) : null;
     }
 
     @Override
     public void saveProject(final Project project) throws IOException, ClassCastException {
         // leave unimplemented for the moment
     }
+    
 }
