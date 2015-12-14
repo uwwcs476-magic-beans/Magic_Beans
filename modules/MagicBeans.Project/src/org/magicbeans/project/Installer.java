d8d457c23062e8671cde78abe8d4f7b129a5b29d
NickBolles <nickbolles@live.com>
2015-12-13 19:48:37 -0600

    add installer and setup wizard that is not used yet

---
+++ modules/MagicBeans.Project/src/org/magicbeans/project/Installer.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.magicbeans.project;
+
+import java.text.MessageFormat;
+import org.openide.DialogDisplayer;
+import org.openide.WizardDescriptor;
+import org.openide.modules.ModuleInstall;
+
+public class Installer extends ModuleInstall {
+
+    @Override
+    public void restored() {
+        // TODO
+//        WizardDescriptor wiz = new WizardDescriptor(new setupWizardIterator());
+//        // {0} will be replaced by WizardDescriptor.Panel.getComponent().getName()
+//        // {1} will be replaced by WizardDescriptor.Iterator.name()
+//        String title = WizardDescriptor.Panel.getComponent().getName();
+//        wiz.setTitleFormat(new MessageFormat("{0} ({1})"));
+//        wiz.setTitle("...dialog title...");
+//        if (DialogDisplayer.getDefault().notify(wiz) == WizardDescriptor.FINISH_OPTION) {
+//            
+//        }
+    }
+
+}
