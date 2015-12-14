a5ad25ae65010d242053946092ac883342bef5c6
NickBolles <nickbolles@live.com>
2015-12-14 11:28:35 -0600

    add the PDFGenerator.java progress

---
+++ modules/MagicBeans.latexFileType/src/latexstudio/editor/pdf/PDFGenerator.java
@@ -0,0 +1,161 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latexstudio.editor.pdf;
+
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.prefs.PreferenceChangeEvent;
+import java.util.prefs.PreferenceChangeListener;
+import java.util.prefs.Preferences;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import latexstudio.editor.files.FileService;
+import latexstudio.editor.runtime.CommandLineBuilder;
+import latexstudio.editor.runtime.CommandLineExecutor;
+import latexstudio.editor.settings.LaTeXSettingsOptionsPanelController;
+import latexstudio.editor.util.ApplicationUtils;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.openide.util.Exceptions;
+import org.openide.util.NbPreferences;
+
+/**
+ *
+ * @author Nicholas
+ */
+public class PDFGenerator {
+    private PDFDisplay display;
+    private String workingDir;
+    private File file;
+    public PDFGenerator(File file) {
+        super();
+        this.workingDir = file.getParentFile().getAbsolutePath();
+        this.file = file;
+        display = new PDFDisplay();
+    }
+    public PDFDisplay getDisplay(){
+        return display;
+    }
+    public void  generate(){
+        CommandLineExecutor.executeGeneratePDF(new CommandLineBuilder()
+                .withPathToSource(file.getAbsolutePath())
+                .withOutputDirectory(ApplicationUtils.getBuildDirectory(workingDir))
+                .withJobname("preview")
+                .withWorkingFile(file.getParentFile())
+                .withLatexPath(LaTeXSettingsOptionsPanelController.getLatexPath())
+        );
+    }
+    
+    private static final double SCALE_FACTOR = 0.4;
+    private static final int SCALE_TYPE = Image.SCALE_SMOOTH;
+    public Image buildPDFPreview(PDPage pdfPage, int zoom) {
+        if (pdfPage == null) {
+            return null;
+        }
+         
+
+        double newScale = SCALE_FACTOR * (((double)zoom)/100.0);
+        BufferedImage pageImage = null;
+        try {
+            pageImage = pdfPage.convertToImage();
+            int width = (int) (newScale * pageImage.getWidth());
+            int height = (int) (newScale * pageImage.getHeight());
+            int type = pageImage.getType();
+            
+            return pageImage.getScaledInstance(width, height, SCALE_TYPE);
+            
+        } catch (IOException ex) {
+            Exceptions.printStackTrace(ex);
+        }
+        
+        return null;
+    }
+    private static PDDocument inputPDF;
+    private static String PDF_PATH ;
+
+    public PDPage getPDFPage(int number) {
+        PDF_PATH = ApplicationUtils.getTempPDFFile(workingDir);
+        
+        PDPage page = null;
+        
+        File pdfFile = null;
+        try {
+            pdfFile = new File(PDF_PATH);
+            if (pdfFile.exists()) {
+                inputPDF = PDDocument.load(pdfFile);
+                List<PDPage> allPages = inputPDF.getDocumentCatalog().getAllPages();
+                if (allPages != null && !allPages.isEmpty() && allPages.size() >= number && number > 0) {
+                    page = allPages.get(number - 1);
+                }
+            }
+        } catch (IOException ex) {
+            
+        }
+        closeDocument();
+        return page;
+    }
+    
+    public static void closeDocument() {
+        if (inputPDF != null) {
+            try {
+                inputPDF.close();
+            } catch (IOException ex) {
+                Exceptions.printStackTrace(ex);
+            }
+        }
+    }
+    
+    public class PDFDisplay {
+
+        private int selectedPage;
+        private int viewZoom;
+
+        public PDFDisplay() {
+            this.selectedPage = 1;
+            this.viewZoom = 100;
+        }
+
+        public JPanel drawPreviewOnJPanel() {
+            JPanel pdfImagePanel = new JPanel();
+
+            Image generatedImage = buildPDFPreview(getPDFPage(selectedPage), viewZoom);
+
+            if (generatedImage != null) {
+                ImageIcon icon = new ImageIcon(generatedImage);
+                JLabel picLabel = new JLabel(icon);
+                pdfImagePanel.add(picLabel);
+            }
+            return pdfImagePanel;
+        }
+
+        public void previousPage() {
+            selectedPage--;
+        }
+
+        public void nextPage() {
+            selectedPage++;
+        }
+
+        public void setPage(int page) {
+            selectedPage = page;
+        }
+
+        public int getSelectedPage() {
+            return selectedPage;
+        }
+
+        public void setZoom(int zoom) {
+            viewZoom = zoom;
+        }
+        public int getZoom () {
+            return viewZoom;
+        }
+    }
+}
