961aeb3de6bff7a889941097a955248da9be2188
AlexLaurent11 <LaurentAM03@uww.edu>
2015-12-20 11:32:13 -0600

    Revert "That didn't do anything"
    This reverts commit b23b205eb96b1547414f81df5fec124a79b00c0f.

--- modules/MagicBeans.latexFileType/src/org/MagicBeans/latexFileType/latexVisualElement.java
+++ modules/MagicBeans.latexFileType/src/org/MagicBeans/latexFileType/latexVisualElement.java
@@ -23,6 +23,7 @@ import org.openide.filesystems.FileEvent;
 import org.openide.filesystems.FileObject;
 import org.openide.util.Lookup;
 import org.openide.util.NbBundle.Messages;
+import org.openide.util.Utilities;
 import org.openide.windows.TopComponent;
 
 @MultiViewElement.Registration(
@@ -55,7 +56,7 @@ public final class latexVisualElement extends JPanel implements MultiViewElement
         //Initialize the PDF Generator for this file
 
 
-        pdfGenerator = new PDFGenerator(new File(obj.getPrimaryFile().toURI()));
+        pdfGenerator = new PDFGenerator(Utilities.toFile(obj.getPrimaryFile().toURI()));
 
 
         //Get the PDfDisplay controller, which handles changing pages, zoom etc.
